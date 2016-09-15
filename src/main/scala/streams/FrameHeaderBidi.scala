084e0af9975fd4bb39b7110b634df1a2ef872ff7
Yohann Brédoux <yohann.bredoux@gmail.com>
2016-09-15 23:21:25 +0200

    perf: send both header and body at once

--- src/main/scala/streams/FrameHeaderBidi.scala
+++ src/main/scala/streams/FrameHeaderBidi.scala
@@ -9,10 +9,14 @@ import cassandra.protocol._
 import cassandra.Encoders._
 import cassandra.decoder._
 
+import scala.concurrent.duration._
+
 object FrameHeaderBidi {
   def framing(implicit system: akka.actor.ActorSystem): BidiFlow[(FrameHeader, Source[ByteString, NotUsed]), ByteString, ByteString, (FrameHeader, Source[ByteString, NotUsed]), NotUsed] = BidiFlow.fromGraph(GraphDSL.create() { implicit b =>
     val outbound = b.add(Flow[(FrameHeader, Source[ByteString, NotUsed])].flatMapConcat { case (fh, source) =>
-      Source.single(fh.toBytes) concat source
+        Source.single(fh.toBytes).concat(source)
+        // try to send frame header and frame body with a same TCP packet
+        .groupedWithin(2, 1 microseconds).map(_.foldLeft(ByteString.empty)(_ ++ _))
     })
 
     val inbound = b.add(Flow.fromGraph(new StreamDetacher(CassandraDecoders.frameHeader)))
